62807f450ab6f3a9c62667dde5ff8ce8c9c79563
Martijn Laarman <Mpdreamz@gmail.com>
2020-05-05 19:11:40 +0200

    add license header to ALL files

--- src/Differ/Providers/IAssemblyProviderFactory.cs
+++ src/Differ/Providers/IAssemblyProviderFactory.cs
@@ -1,4 +1,8 @@
-﻿namespace Differ.Providers
+﻿// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+namespace Differ.Providers
 {
 	public interface IAssemblyProviderFactory
 	{
--- src/Elastic.Xunit/XunitPlumbing/IClusterFixture.cs
+++ src/Elastic.Xunit/XunitPlumbing/IClusterFixture.cs
@@ -1,4 +1,8 @@
-﻿using Elastic.Managed;
+﻿// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using Elastic.Managed;
 using Elastic.Managed.Ephemeral;
 
 namespace Elastic.Xunit.XunitPlumbing
