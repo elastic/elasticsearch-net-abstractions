31ff9a7fb4ebd49d689612592e8d856cc1d17cc9
Martijn Laarman <Mpdreamz@gmail.com>
2023-06-29 15:19:07 +0200

    Added ephemeral example

---
+++ examples/Elastic.Ephemeral.Example/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Elastic.Elasticsearch.Ephemeral;
+
+var config = new EphemeralClusterConfiguration("8.7.0");
+var cluster = new EphemeralCluster(config);
+using var started = cluster.Start();
