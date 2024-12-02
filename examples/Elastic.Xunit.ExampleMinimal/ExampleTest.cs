80e74c026c214609b772a5d12124e472bbd0f17a
Martijn Laarman <Mpdreamz@gmail.com>
2024-12-02 16:56:21 +0100

    more debugging

--- examples/Elastic.Xunit.ExampleMinimal/ExampleTest.cs
+++ examples/Elastic.Xunit.ExampleMinimal/ExampleTest.cs
@@ -21,7 +21,7 @@ namespace Elastic.Xunit.ExampleMinimal
 		///     We pass our configuration instance to the base class.
 		///     We only configure it to run version 6.2.3 here but lots of additional options are available.
 		/// </summary>
-		public MyTestCluster() : base(new XunitClusterConfiguration("8.16.0")
+		public MyTestCluster() : base(new XunitClusterConfiguration("8.14.0")
 			{ PrintYamlFilesInConfigFolder = true, NoCleanupAfterNodeStopped = true })
 		{
 		}
